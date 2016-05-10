104f508412d15be4ccf67924672c49b61a8fd368
Ingvar Stepanyan <me@rreverser.com>
2016-05-10 16:02:08 +0100

    Prettify bench code.

--- bench/index.js
+++ bench/index.js
@@ -3,38 +3,38 @@
 var Benchmark = require('benchmark');
 var MyTokenizer = require('../js-tokenizer').HtmlTokenizer;
 var Parse5Tokenizer = require('parse5/lib/tokenizer');
-var text = require('fs').readFileSync(`${__dirname}/huge-page.html`, 'utf-8');
-var text2 = require('fs').readFileSync(`${__dirname}/huge-page-2.html`, 'utf-8');
+var read = require('fs').readFileSync;
+var text;
 
-new Benchmark.Suite()
-    .add('my #1', function() {
+var suite =
+    new Benchmark.Suite()
+    .add('my', () => {
         new MyTokenizer({
             onToken() {}
         }).feed(text, true);
     })
-    .add('parse5 #1', function() {
+    .add('parse5', () => {
         var tokenizer = new Parse5Tokenizer();
         tokenizer.write(text, true);
         do {
             var token = tokenizer.getNextToken();
         } while (token.type !== 'EOF_TOKEN');
     })
-    .add('my #2', function() {
-        new MyTokenizer({
-            onToken() {}
-        }).feed(text2, true);
-    })
-    .add('parse5 #2', function() {
-        var tokenizer = new Parse5Tokenizer();
-        tokenizer.write(text2, true);
-        do {
-            var token = tokenizer.getNextToken();
-        } while (token.type !== 'EOF_TOKEN');
-    })
-    .on('error', function(event) {
+    .on('error', event => {
         throw event.target.error;
     })
-    .on('cycle', function(event) {
-        console.log(String(event.target));
-    })
-    .run();
+    .on('cycle', event => console.log(String(event.target)));
+
+function logAndUnderline(text, char) {
+    console.log(text);
+    console.log(char.repeat(text.length));
+}
+
+['huge-page.html', 'huge-page-2.html'].forEach(src => {
+    var fill = '='.repeat(src.length);
+    console.log(fill);
+    console.log(src);
+    console.log(fill);
+    text = read(`${__dirname}/${src}`, 'utf-8');
+    suite.run();
+});
