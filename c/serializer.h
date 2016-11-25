149cc50ebd43b106e0f35a9417b8808d4feda306
Ingvar Stepanyan <me@rreverser.com>
2016-11-25 14:19:00 +0000

    Make serializer's callback argument typesafe
    Caller can cast pointer from lhtml_serializer_state_t if needed.

--- c/serializer.h
+++ c/serializer.h
@@ -3,13 +3,15 @@
 
 #include "tokenizer.h"
 
-typedef void (*lhtml_string_callback_t)(lhtml_string_t string, void *extra);
+typedef struct lhtml_serializer_state_s lhtml_serializer_state_t;
 
-typedef struct {
+typedef void (*lhtml_string_callback_t)(lhtml_string_t string, lhtml_serializer_state_t *extra);
+
+struct lhtml_serializer_state_s {
     lhtml_token_handler_t handler; // needs to be the first one
     lhtml_string_callback_t writer;
     bool compact;
-} lhtml_serializer_state_t;
+};
 
 __attribute__((nonnull))
 void lhtml_serializer_inject(lhtml_state_t *tokenizer, lhtml_serializer_state_t *state);
