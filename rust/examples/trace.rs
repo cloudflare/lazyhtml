4f3f9e35efe3f1015c45253bb10d7fbd65e82803
Ingvar Stepanyan <me@rreverser.com>
2017-10-05 17:03:24 +0100

    Add basic options to tracer, remove it from C

--- rust/examples/trace.rs
+++ rust/examples/trace.rs
@@ -1,9 +1,12 @@
+extern crate getopts;
 extern crate lazyhtml;
 
 use std::mem::zeroed;
 use std::ptr::{null, null_mut};
 use lazyhtml::*;
 use std::os::raw::{c_char, c_void};
+use getopts::Options;
+use std::env::args;
 
 struct HandlerState {
     handler: lhtml_token_handler_t,
@@ -26,16 +29,64 @@ impl HandlerState {
 }
 
 fn main() {
+    let mut opts = Options::new();
+
+    opts.optflag("f", "feedback", "Enable parser feedback");
+    opts.optopt(
+        "s",
+        "state",
+        "Initial state",
+        "-s (Data|PlainText|RCData|RawText|ScriptData|CDataSection)",
+    );
+    opts.optflag("h", "help", "Show this help");
+
+    let matches = match opts.parse(args().skip(1)) {
+        Ok(matches) => if matches.free.is_empty() {
+            eprintln!("Missing HTML input");
+            None
+        } else if matches.opt_present("h") {
+            None
+        } else {
+            Some(matches)
+        },
+        Err(e) => {
+            eprintln!("{}", e);
+            None
+        }
+    };
+
+    let matches = match matches {
+        Some(m) => m,
+        None => {
+            eprintln!("{}", opts.usage("Usage: trace [options] INPUT"));
+            return;
+        }
+    };
+
+    let initial_state = match matches.opt_str("s").as_ref().map(|s| s.as_str()) {
+        None | Some("Data") => html_en_Data,
+        Some("PlainText") => html_en_PlainText,
+        Some("RCData") => html_en_RCData,
+        Some("RawText") => html_en_RawText,
+        Some("ScriptData") => html_en_ScriptData,
+        Some("CDataSection") => html_en_CDataSection,
+        _ => {
+            eprintln!("Unknown state, defaulting to Data");
+            html_en_Data
+        }
+    };
+
+    let with_feedback = matches.opt_present("f");
+
+    let input = matches.free.first().unwrap();
+
     unsafe {
         let buffer: [c_char; 2048] = zeroed();
         let attr_buffer: [lhtml_attribute_t; 256] = zeroed();
         let ns_buffer: [lhtml_ns_t; 64] = zeroed();
 
-        let initial_state = html_en_Data;
-        let with_feedback = false;
-
         let mut tokenizer = lhtml_state_t {
-            cs: initial_state as _,
+            cs: initial_state,
             buffer: lhtml_buffer_t {
                 data: buffer.as_ptr(),
                 capacity: buffer.len(),
@@ -68,8 +119,6 @@ fn main() {
 
         let mut test_state = HandlerState::new();
         lhtml_append_handlers(&mut tokenizer.base_handler, &mut test_state.handler);
-
-        let input = ::std::env::args().nth(1).expect("Provide HTML snippet");
 
         assert!(lhtml_feed(
             &mut tokenizer,
