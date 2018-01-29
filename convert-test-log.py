09f6dd3254f71e792c059219a04c5007ac4d5945
Ingvar Stepanyan <me@rreverser.com>
2018-01-29 20:44:17 +0000

    Try to parse failure details into jUnit XML

--- convert-test-log.py
+++ convert-test-log.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python
 import xml.etree.cElementTree as ET
+import re
 
-lines = open('test.log', 'r').read().rstrip().split('\n')
+failure_re = re.compile(r"""thread '(.*?)' panicked at '(.*?)', tests/test\.rs:\d+:\d+
+(?:note: Run with `RUST_BACKTRACE=1` for a backtrace.
+)?failed \1\n""", re.DOTALL)
 
-root = ET.Element('testsuite', name='html5lib-tests', tests=str(len(lines)))
+success_re = re.compile("(ok|ignored) (.*?)\n")
 
-for line in lines:
-    (status, name) = line.split(' ', 1)
-    case = ET.SubElement(root, 'testcase', name=name)
-    if status == 'failed':
-        ET.SubElement(case, 'failure')
-    elif status == 'ignored':
-        ET.SubElement(case, 'skipped')
+log = open('test.log', 'r').read()
+
+root = ET.Element('testsuite', name='html5lib-tests')
+
+pos = 0
+while pos < len(log):
+    match = success_re.match(log, pos)
+    if match is not None:
+        status, name = match.groups()
+        case = ET.SubElement(root, 'testcase', name=name)
+        if status == 'ignored':
+            ET.SubElement(case, 'skipped')
     else:
-        assert status == 'ok', 'Unknown test status: %s' % status
+        match = failure_re.match(log, pos)
+        assert match is not None, "Could not parse %r" % log[pos:].split('\n', 1)[0]
+        name, msg = match.groups()
+        case = ET.SubElement(root, 'testcase', name=name)
+        ET.SubElement(case, 'failure').text = msg
+    pos = match.end()
 
 ET.ElementTree(root).write('test.xml')
